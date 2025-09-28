40b74d9dcd8ecbdbf74a03818c73d11d0fba1cd5
MuhammadBek97 <allayevmuhammad8@gmail.com>
2025-09-28 10:34:34 +0500

    connecting backend

--- src/Context.jsx
+++ src/Context.jsx
@@ -16,7 +16,7 @@ import {
 
 // API base URL configuration
 const API_BASE_URL = import.meta.env.DEV 
-  ? "http://localhost:3007/api"
+  ? "http://localhost:3008/api"
   : import.meta.env.VITE_API_BASE_URL;
 
 const AppContext = createContext();
